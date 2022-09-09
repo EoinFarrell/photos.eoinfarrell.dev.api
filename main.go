48920c1dd94d3facf1671dc8cd01717177211204
EoinFarrell <efarrell@zendesk.com>
2022-09-09 10:50:53 +0100

    Add DB read support and endpoints

--- main.go
+++ main.go
@@ -1,67 +1,169 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type imageInfo struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+
+	dbTest()
+
+	router.GET("/images", getImages)
+	router.GET("/images/:id", getImageByID)
 
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getDbConnection() *sql.DB {
+	host := os.Getenv("DIGIKAM_DB_HOST")
+	port := os.Getenv("DIGIKAM_DB_PORT")
+	username := os.Getenv("DIGIKAM_DB_USER")
+	password := os.Getenv("DIGIKAM_DB_USER_PASSWORD")
+	dbName := os.Getenv("DIGIKAM_DB_NAME")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbName)
+
+	// create a database object which can be used
+	// to connect with database.
+	db, err := sql.Open("mysql", connectionString)
+
+	// handle error, if any.
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+func dbTest() {
+	db := getDbConnection()
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
+	// Here a SQL query is used to return all
+	// the data from the table user.
+	result, err := db.Query("SELECT id, name FROM digikam.Images limit 3")
+
+	// handle error
+	if err != nil {
+		panic(err)
+	}
+
+	// the result object has a method called Next,
+	// which is used to iterate through all returned rows.
+	for result.Next() {
+
+		var id int
+		var name string
+
+		// The result object provided Scan  method
+		// to read row data, Scan returns error,
+		// if any. Here we read id and name returned.
+		err = result.Scan(&id, &name)
+
+		// handle error
+		if err != nil {
+			panic(err)
 		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
+		fmt.Printf("Id: %d Name: %s\n", id, name)
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	// database object has  a method Close,
+	// which is used to free the resource.
+	// Free the resource when the function
+	// is returned.
+	defer db.Close()
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func getImagesFromDb() []imageInfo {
+	db := getDbConnection()
+
+	// Here a SQL query is used to return all
+	// the data from the table user.
+	rows, err := db.Query("SELECT id, name FROM digikam.Images limit 3")
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return handleImageDbRows(rows)
+}
+
+func getImagesFromDbByTag(tag string) []imageInfo {
+	db := getDbConnection()
+
+	// Here a SQL query is used to return all
+	// the data from the table user.
+	rows, err := db.Query(fmt.Sprintf("select i.id, i.name from digikam.Tags t join digikam.ImageTags it on t.id = it.tagid join digikam.Images i on it.imageid = i.id where t.name = '%s' limit 3;", tag))
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return handleImageDbRows(rows)
+}
+
+func getImageFromDbById(id string) []imageInfo {
+	db := getDbConnection()
+
+	// Here a SQL query is used to return all
+	// the data from the table user.
+	rows, err := db.Query(fmt.Sprintf("select id, name from digikam.Images where id = '%s';", id))
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return handleImageDbRows(rows)
+}
+
+func handleImageDbRows(rows *sql.Rows) []imageInfo {
+	var image imageInfo
+	var images []imageInfo
+
+	// the result object has a method called Next,
+	// which is used to iterate through all returned rows.
+	for rows.Next() {
+		err := rows.Scan(&image.ID, &image.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(image.ID, image.Name)
+		images = append(images, image)
+	}
+
+	return images
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getImages(c *gin.Context) {
+	tag := c.Query("tag")
+
+	if tag != "" {
+		c.IndentedJSON(http.StatusOK, getImagesFromDbByTag(tag))
+	} else {
+		c.IndentedJSON(http.StatusOK, getImagesFromDb())
+	}
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getImageByID(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, getImageFromDbById(c.Param("id")))
 }
